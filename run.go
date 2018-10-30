6feda215dd67725fae4bb7625ed3f44cb37cdb1d
Roberto Mier Escandon <rmescandon@gmail.com>
2018-10-30 16:23:52 +0100

    Simplify error management into the container

--- run.go
+++ run.go
@@ -46,13 +46,6 @@ func init() {
 	}
 }
 
-/*
-Configurable items:
-- prompt
-- rootfs
-- uts mappings
-*/
-
 // Run runs command in a new container
 func Run(args []string) error {
 	if len(args) == 0 {
@@ -115,32 +108,20 @@ func reexec(args []string) {
 	// Setup container hostname
 	// (It'll be shown as container prompt)
 	hostname := fmt.Sprintf("[container-%v] # ", randStr(6))
-	if err := syscall.Sethostname([]byte(hostname)); err != nil {
-		fmt.Printf("Could not set the hostname - %s\n", err)
-		os.Exit(1)
-	}
+	assert(syscall.Sethostname([]byte(hostname)))
 
 	c, err := loadCfg()
-	if err != nil {
-		fmt.Printf("Could not read settings - %s\n", err)
-		os.Exit(1)
-	}
+	assert(err)
 
 	// Mount /proc.
 	// This MUST be done BEFORE PIVOTING. Otherwise it wont be allowed to do it.
 	// (now you can check that ps reports only container processes ids
 	// and that ls -lah /proc/mounts reports only container mounts but not host's)
-	if err := mountProc(c.Rootfs); err != nil {
-		fmt.Printf("Could not mount /proc on the new rootfs - %s\n", err)
-		os.Exit(1)
-	}
+	assert(mountProc(c.Rootfs))
 
 	// Pivot root to configured rootfs
 	// (now you can check that we have moved to the new path)
-	if err := pivotRoot(c.Rootfs); err != nil {
-		fmt.Printf("Could not pivot to the new rootfs - %s\n", err)
-		os.Exit(1)
-	}
+	assert(pivotRoot(c.Rootfs))
 
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Stdin = os.Stdin
@@ -148,21 +129,12 @@ func reexec(args []string) {
 	cmd.Stderr = os.Stderr
 	cmd.Env = []string{fmt.Sprintf("PS1=%v", hostname)}
 
-	if err := cmd.Start(); err != nil {
-		fmt.Printf("Could not start command execution - %s\n", err)
-		os.Exit(1)
-	}
+	assert(cmd.Start())
 
 	// Configure veth1 interface
-	if err := configureNetworkOnContainer(c.Network.Veth, c.Network.CIDR, cmd.Process.Pid); err != nil {
-		fmt.Printf("Could not configure network on container - %s\n", err)
-		os.Exit(1)
-	}
+	assert(configureNetworkOnContainer(c.Network.Veth, c.Network.CIDR, cmd.Process.Pid))
 
-	if err := cmd.Wait(); err != nil {
-		fmt.Printf("Error waiting for the command execution to finish - %s\n", err)
-		os.Exit(1)
-	}
+	assert(cmd.Wait())
 }
 
 func assert(err error) {
